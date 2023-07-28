24e4a3f1e1b9a0d2dffc73f7a4e1193950b2cc0d
Dikshita Agarwal <quic_dikshita@quicinc.com>
2023-07-28 18:53:37 +0530

    iris: platform: sm8550: add capability file for sm8550
    This implements all the capabilities supported by sm8550.
    
    Signed-off-by: Dikshita Agarwal <quic_dikshita@quicinc.com>
    Signed-off-by: Vikash Garodia <quic_vgarodia@quicinc.com>

---
+++ drivers/media/platform/qcom/iris/platform/sm8550/inc/msm_vidc_sm8550.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (c) 2020-2021, The Linux Foundation. All rights reserved.
+ * Copyright (c) 2021-2023 Qualcomm Innovation Center, Inc. All rights reserved.
+ */
+
+#ifndef _MSM_VIDC_SM8550_H_
+#define _MSM_VIDC_SM8550_H_
+
+#include "msm_vidc_core.h"
+
+int msm_vidc_init_platform_sm8550(struct msm_vidc_core *core);
+
+#endif // _MSM_VIDC_SM8550_H_
