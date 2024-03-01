8eba1fd676076be044bb76276dc9d14beb12b2a5
Xilin Wu <wuxilin123@gmail.com>
2024-03-01 11:56:49 +0800

    HACK: wifi: ath12k: Always try to suspend when requested
    Signed-off-by: Xilin Wu <wuxilin123@gmail.com>

--- drivers/net/wireless/ath/ath12k/core.c
+++ drivers/net/wireless/ath/ath12k/core.c
@@ -44,8 +44,8 @@ int ath12k_core_suspend(struct ath12k_base *ab)
 {
 	int ret;
 
-	if (!ab->hw_params->supports_suspend)
-		return -EOPNOTSUPP;
+	// if (!ab->hw_params->supports_suspend)
+	// 	return -EOPNOTSUPP;
 
 	/* TODO: there can frames in queues so for now add delay as a hack.
 	 * Need to implement to handle and remove this delay.
@@ -82,8 +82,8 @@ int ath12k_core_resume(struct ath12k_base *ab)
 {
 	int ret;
 
-	if (!ab->hw_params->supports_suspend)
-		return -EOPNOTSUPP;
+	// if (!ab->hw_params->supports_suspend)
+	// 	return -EOPNOTSUPP;
 
 	ret = ath12k_hif_resume(ab);
 	if (ret) {
