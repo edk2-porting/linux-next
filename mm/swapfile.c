62e384c4fd902fc1cdc2f31dd81fb5f49c7a7270
Matthew Wilcox (Oracle) <willy@infradead.org>
2024-08-07 20:37:32 +0100

    mm: return the folio from swapin_readahead
    The unuse_pte_range() caller only wants the folio while do_swap_page()
    wants both the page and the folio.  Since do_swap_page() already has logic
    for handling both the folio and the page, move the folio-to-page logic
    there.  This also lets us allocate larger folios in the SWP_SYNCHRONOUS_IO
    path in future.
    
    Link: https://lkml.kernel.org/r/20240807193734.1865400-1-willy@infradead.org
    Signed-off-by: Matthew Wilcox (Oracle) <willy@infradead.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>

--- mm/swapfile.c
+++ mm/swapfile.c
@@ -2124,7 +2124,6 @@ static int unuse_pte_range(struct vm_area_struct *vma, pmd_t *pmd,
 
 		folio = swap_cache_get_folio(entry, vma, addr);
 		if (!folio) {
-			struct page *page;
 			struct vm_fault vmf = {
 				.vma = vma,
 				.address = addr,
@@ -2132,10 +2131,8 @@ static int unuse_pte_range(struct vm_area_struct *vma, pmd_t *pmd,
 				.pmd = pmd,
 			};
 
-			page = swapin_readahead(entry, GFP_HIGHUSER_MOVABLE,
+			folio = swapin_readahead(entry, GFP_HIGHUSER_MOVABLE,
 						&vmf);
-			if (page)
-				folio = page_folio(page);
 		}
 		if (!folio) {
 			swp_count = READ_ONCE(si->swap_map[offset]);
