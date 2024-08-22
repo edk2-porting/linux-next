f0d2dcc9b087998b146c735db725c8a4561fef9a
Ulf Hansson <ulf.hansson@linaro.org>
2024-08-23 00:45:41 +0200

    OPP/pmdomain: Set the required_dev for a required OPP during genpd attach
    Through dev_pm_opp_set_config() the _opp_attach_genpd() allows consumer
    drivers to attach a device to its PM domains. Their corresponding virtual
    devices that are created by genpd during attach, are later being assigned
    as the required_devs for the corresponding required OPPs. In principle,
    using dev_pm_opp_set_config() for this works fine.
    
    However, attaching a device to its PM domains is in general better done
    with dev_pm_domain_attach|detach_list(). To avoid having two different ways
    to manage this, let's prepare for the removal of _opp_attach_genpd().
    
    More precisely, let's assign the required_devs during device attach in
    genpd, by using _opp_set_required_dev(). In this way, the consumer driver
    can rely solely on dev_pm_domain_attach|detach_list() to attach its device
    to its PM domains.
    
    Note that, we still need to allow existing users of _opp_attach_genpd(),
    which makes this change slightly more complicated. On the other hand, once
    we have migrated those users to dev_pm_domain_attach|detach_list(), the
    entire _opp_attach_genpd() can be removed.
    
    Signed-off-by: Ulf Hansson <ulf.hansson@linaro.org>
    Acked-by: Viresh Kumar <viresh.kumar@linaro.org>
    Link: https://lore.kernel.org/r/20240822224547.385095-5-ulf.hansson@linaro.org

--- drivers/pmdomain/core.c
+++ drivers/pmdomain/core.c
@@ -2884,6 +2884,61 @@ static void genpd_dev_pm_sync(struct device *dev)
 	genpd_queue_power_off_work(pd);
 }
 
+static struct opp_table *genpd_find_opp_table(struct generic_pm_domain *genpd,
+					      unsigned int depth)
+{
+	struct opp_table *opp_table;
+	struct gpd_link *link;
+
+	if (genpd->opp_table)
+		return genpd->opp_table;
+
+	list_for_each_entry(link, &genpd->child_links, child_node) {
+		struct generic_pm_domain *parent = link->parent;
+
+		genpd_lock_nested(parent, depth + 1);
+		opp_table = genpd_find_opp_table(parent, depth + 1);
+		genpd_unlock(parent);
+
+		if (opp_table)
+			return opp_table;
+	}
+
+	return NULL;
+}
+
+static int genpd_set_required_opp_dev(struct device *dev,
+				      struct device *base_dev)
+{
+	struct generic_pm_domain *genpd = dev_to_genpd(dev);
+	struct opp_table *opp_table;
+	int ret = 0;
+
+	/* Limit support to non-providers for now. */
+	if (of_property_present(base_dev->of_node, "#power-domain-cells"))
+		return 0;
+
+	if (!dev_pm_opp_of_has_required_opp(base_dev))
+		return 0;
+
+	genpd_lock(genpd);
+	opp_table = genpd_find_opp_table(genpd, 0);
+	genpd_unlock(genpd);
+
+	if (opp_table) {
+		struct dev_pm_opp_config config = {
+			.required_dev = dev,
+			.required_opp_table = opp_table,
+		};
+
+		ret = devm_pm_opp_set_config(base_dev, &config);
+		if (ret < 0)
+			dev_err(dev, "failed to set opp config %d\n", ret);
+	}
+
+	return ret;
+}
+
 static int genpd_set_required_opp(struct device *dev, unsigned int index)
 {
 	int ret, pstate;
@@ -2939,6 +2994,10 @@ static int __genpd_dev_pm_attach(struct device *dev, struct device *base_dev,
 
 	dev->pm_domain->detach = genpd_dev_pm_detach;
 	dev->pm_domain->sync = genpd_dev_pm_sync;
+
+	ret = genpd_set_required_opp_dev(dev, base_dev);
+	if (ret)
+		goto err;
 
 	ret = genpd_set_required_opp(dev, index);
 	if (ret)
